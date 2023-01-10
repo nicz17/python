fb72bb95ca895e992926957216fae9b706b94754
nicz17 <nicolas.zwahlen@gmx.net>
2023-01-10 19:14:03 +0100

    Add HtmlPage test case

--- Orfact/orfact.py
+++ Orfact/orfact.py
@@ -12,6 +12,7 @@ from NameGen import *
 from Ortifact import *
 from OrtifactGen import *
 from Palette import *
+from HtmlPage import *
 
 def log(msg):
     print(msg, file=sys.stdout, flush=True)
@@ -40,9 +41,34 @@ def testPalette():
     pal = RandomPalette()
     pal.toColorScale('RandomPalette.png', 800, 50)
 
+def testHtmlPage():
+    log('Testing HtmlPage')
+    page = HtmlPage('Test', 'orfact.css')
+    page.addHeading(1, 'Image generation tests')
+
+    page.addHeading(2, 'Palettes')
+    aNames = ['HeatPalette', 'OraVioPalette', 'RandomPalette']
+    aPalImgs = []
+    for sName in aNames:
+        tImg = HtmlTag('img')
+        tImg.addAttr('src', sName + '.png')
+        tImg.addAttr('title', sName)
+        aPalImgs.append(tImg)
+    page.addTable(aPalImgs, 1)
+
+    page.addHeading(2, 'Random names')
+    nameGen = NameGen(42)
+    aNames = []
+    for i in range(12):
+        aNames.append(HtmlTag('p', nameGen.generate()))
+    page.addTable(aNames)
+
+    page.save('Images.html')
+
 def main():
     log('Welcome to Orfact v' + __version__)
-    testOrtifactGen()
+    #testOrtifactGen()
     testPalette()
+    testHtmlPage()
 
 main()
