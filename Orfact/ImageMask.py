abbac226052b30f0df46a454f1cec2904b85e3f2
nicz17 <nicolas.zwahlen@gmx.net>
2023-01-11 21:48:39 +0100

    Add WaveImageMask

--- Orfact/ImageMask.py
+++ Orfact/ImageMask.py
@@ -21,7 +21,6 @@ class ImageMask:
 
     def generate(self):
         print('Generating', self.__str__())
-        self.aMask = np.random.rand(self.w, self.h)
 
     def toImage(self, oPalette, sFilename):
         print('Saving', self.__str__(), 'as', sFilename, 'with palette', oPalette.sName)
@@ -41,6 +40,14 @@ class ImageMask:
 
     def __str__(self) -> str:
         return self.sName + ' ' + str(self.w) + 'x' + str(self.h)
+
+class RandomImageMask(ImageMask):
+    def __init__(self, w, h):
+        super().__init__('RandomImageMask', w, h)
+
+    def generate(self):
+        print('Generating', self.__str__())
+        self.aMask = np.random.rand(self.w, self.h)
 
 class LinearImageMask(ImageMask):
     def __init__(self, w, h):
@@ -79,7 +86,19 @@ class ManhattanImageMask(ImageMask):
                 dx = abs(x - self.w/2)
                 dy = abs(y - self.h/2)
                 dist = dx + dy
-                self.aMask[x, y] = GaussImageMask.gauss(dist, 0.0, self.w/4.0)
+                self.aMask[x, y] = ManhattanImageMask.gauss(dist, 0.0, self.w/4.0)
 
     def gauss(x, mu, sig):
         return math.exp(-(x-mu)*(x-mu)/(sig*sig))
+
+class WaveImageMask(ImageMask):
+    def __init__(self, freq, w, h):
+        super().__init__('WaveImageMask', w, h)
+        self.freq = freq
+
+    def generate(self):
+        print('Generating', self.__str__())
+        for x in range(self.w):
+            for y in range(self.h):
+                self.aMask[x, y] = 0.25*(2.0 + math.sin(self.freq*x) + math.cos(self.freq*y))
+                #self.aMask[x, y] = 0.5*(1.0 + math.sin(self.freq*(x+y)))  # diagonals
