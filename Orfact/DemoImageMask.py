6b09dae556c4bbbfe060ff53b2d37654886ccf91
nicz17 <nicolas.zwahlen@gmx.net>
2023-01-12 19:04:16 +0100

    Add demo title and timer

--- Orfact/DemoImageMask.py
+++ Orfact/DemoImageMask.py
@@ -7,22 +7,28 @@ __author__ = "Nicolas Zwahlen"
 __copyright__ = "Copyright 2023 N. Zwahlen"
 __version__ = "1.0.0"
 
-import sys
-
+import os
+import time
 from Palette import *
 from ImageMask import *
 from HtmlPage import *
 
 class DemoImageMask:
     def __init__(self) -> None:
+        self.sTitle = 'ImageMask Demo'
         self.size = 300
         self.dir = 'images/'
 
     def run(self):
-        print('Running ImageMask demo')
+        print('Running', self.sTitle)
+        tStart = time.time()
+        
+        # Add dir if missing
+        if not os.path.exists(self.dir):
+            os.makedirs(self.dir)
 
         # Define palettes and create their color scales
-        aPals = [GrayScalePalette(), LinesPalette(), HeatPalette(), RandomPalette()]
+        aPals = [GrayScalePalette(), LinesPalette(), HeatPalette(), GhostPalette(), RandomPalette()]
         aPalImgs = []
         for oPal in aPals:
             sFilename = self.dir + oPal.sName + '.png'
@@ -42,13 +48,17 @@ class DemoImageMask:
                 aMaskImgs.append(ImageHtmlTag(sFilename, oMask.sName + ' rendered with ' + oPal.sName))
 
         # Create HTML page for rendering
-        oPage = HtmlPage('ImageMask Demo', 'orfact.css')
-        oPage.addHeading(1, 'ImageMask Demo')
+        oPage = HtmlPage(self.sTitle, 'orfact.css')
+        oPage.addHeading(1, self.sTitle)
 
         oPage.addHeading(2, 'Palettes')
-        oPage.addTable(aPalImgs)
+        oPage.addTable(aPalImgs, len(aPals))
 
         oPage.addHeading(2, 'Image masks')
         oPage.addTable(aMaskImgs, len(aPals))
 
         oPage.save('ImageMaskDemo.html')
+
+        # Done
+        tEnd = time.time()
+        print(self.sTitle, 'done in %.3fs' % (tEnd-tStart))
