b7a0a7ab318512c234bb30f145993be4fc78be81
nicz17 <nicolas.zwahlen@gmx.net>
2023-01-22 13:42:57 +0100

    Upload base files and photos

--- Recettes/Uploader.py
+++ Recettes/Uploader.py
@@ -9,6 +9,7 @@ import ftplib
 import getpass
 import glob
 import os
+import time
 
 class Uploader:
     """Upload files to website using FTP."""
@@ -16,9 +17,19 @@ class Uploader:
 
     def __init__(self) -> None:
         self.oSession = None
+        self.tLastUpload = self.getLastUpload()
+        self.log.info('Last upload at %s', self.timeToStr(self.tLastUpload))
 
     def uploadBaseFiles(self):
-        pass
+        """Uploads the base files like index, thanks, biblio etc."""
+        self.log.info('Uploading base files')
+        aBaseFiles = ['index.html', 'biblio.html', 'ingredients.html',
+                      'news.html', 'readme.html', 'thanks.html', 'thumbs.html']
+        for sName in aBaseFiles:
+            sFile = config.sDirExport + sName
+            if (self.needsUpload(sFile)):
+                self.log.info('  %s has been modified, will upload', sName)
+                self.upload(sFile)
 
     def uploadChapters(self):
         """Upload all the chapters HTML files."""
@@ -26,6 +37,32 @@ class Uploader:
         self.log.info('Uploading %d chapters', len(aChapters))
         for sChap in aChapters:
             self.upload(sChap)
+
+    def uploadPhotos(self):
+        """Upload the recipe JPG files."""
+        aPhotos = glob.glob(config.sDirPhotos + '*.jpg')
+        self.log.info('Uploading from %d photos', len(aPhotos))
+        if self.oSession:
+            self.oSession.cwd('photos/')
+        for sFile in aPhotos:
+            if (self.needsUpload(sFile)):
+                self.log.info('  %s has been modified, will upload', sFile)
+                self.upload(sFile)
+        if self.oSession:
+            self.oSession.cwd('../')
+
+    def uploadThumbs(self):
+        """Upload the recipe thumbnail files."""
+        aFiles = glob.glob(config.sDirThumbs + '*.jpg')
+        self.log.info('Uploading from %d thumbs', len(aFiles))
+        if self.oSession:
+            self.oSession.cwd('thumbs/')
+        for sFile in aFiles:
+            if (self.needsUpload(sFile)):
+                self.log.info('  %s has been modified, will upload', sFile)
+                self.upload(sFile)
+        if self.oSession:
+            self.oSession.cwd('../')
 
     def upload(self, sFilename):
         """Upload the specified file to FTP."""
@@ -35,13 +72,18 @@ class Uploader:
         oFile.close() 
 
     def test(self):
-        self.log.info('Testing upload of biblio')
+        self.log.info('Testing upload of thanks')
         self.connect()
-        #self.upload(config.sDirExport + 'biblio.html')
-        self.uploadChapters()
+        #self.upload(config.sDirExport + 'thanks.html')
+        #self.uploadChapters()
+        #self.setLastUpload()
+        #self.uploadBaseFiles()
+        self.uploadPhotos()
+        self.uploadThumbs()
         self.quit()
 
     def connect(self):
+        """Connect to FTP server. Ask user for password."""
         self.log.info('Connecting to FTP %s as %s', config.sFtpAddress, config.sFtpUser)
         sFtpPasswd = getpass.getpass(prompt = 'FTP password: ')
         try:
@@ -50,10 +92,27 @@ class Uploader:
             self.oSession.cwd('httpdocs/recettes/')
         except ftplib.error_perm:
             self.log.error('Failed to connect or cwd')
-        finally:
             self.quit()
 
     def quit(self):
+        """Close the connection if it is still open."""
         if self.oSession:
             self.log.info('Closing FTP connection')
             self.oSession.close()
+
+    def needsUpload(self, sFile):
+        #self.log.info('  %s modifed at %s', sFile, self.timeToStr(self.getModifiedAt(sFile)))
+        return self.getModifiedAt(sFile) > self.tLastUpload
+
+    def setLastUpload(self):
+        self.log.info('Setting last-upload timestamp file to now')
+        #os.system('touch last-upload')
+
+    def getLastUpload(self):
+        return self.getModifiedAt('last-upload')
+    
+    def getModifiedAt(self, sFile):
+        return os.path.getmtime(sFile)
+    
+    def timeToStr(self, tAt):
+        return time.strftime('%Y.%m.%d %H:%M:%S', time.gmtime(tAt))
