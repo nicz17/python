52cd4e334e5eeb03d8b82cd923e5db60ab66a5ee
Nicolas Zwahlen <nicolas.zwahlen@gmx.net>
2023-01-24 14:19:09 +0100

    Basic polar image masks

---
+++ Orfact/PolarMask.py
@@ -0,0 +1,83 @@
+"""
+ Subclasses of SimulationMask based on polar coordinates.
+"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2023 N. Zwahlen"
+__version__ = "1.0.0"
+
+import numpy as np
+import math
+import random
+from scipy.ndimage.filters import gaussian_filter
+from ImageMask import ImageMask
+from SimulationMask import SimulationMask
+
+class PolarImageMask(SimulationMask):
+    """A SimulationMask based on polar coordinates."""
+
+    def __init__(self, sName, w, h):
+        super().__init__(sName, w, h)
+        self.aMask = np.zeros((w, h))
+
+    def getRho(self, x, y):
+        """Get the normalized distance from image center."""
+        dx = x - self.w/2
+        dy = y - self.h/2
+        return math.sqrt(dx*dx + dy*dy)/(float(self.h/2))
+
+    def getPhi(self, x, y):
+        """Get the angle from image center."""
+        dx = x - self.w/2
+        dy = y - self.h/2
+        return math.atan2(dy, dx)
+
+class RadarImageMask(PolarImageMask):
+    """Simple polar image."""
+    def __init__(self, w, h):
+        super().__init__('RadarImageMask', w, h)
+
+    def runSimulation(self):
+        for x in range(self.w):
+            for y in range(self.h):
+                rho = self.getRho(x, y)
+                phi = self.getPhi(x, y)
+                self.aMask[x, y] = phi + 3.0*rho
+
+class SpiralImageMask(PolarImageMask):
+    """A spiral."""
+    def __init__(self, w, h):
+        super().__init__('SpiralImageMask', w, h)
+        self.freq = 7.0
+        self.curv = 13.0
+
+    def randomize(self):
+        super().randomize()
+        self.freq = random.randrange(4, 16)
+        self.curv = random.randrange(4, 16)
+
+    def runSimulation(self):
+        for x in range(self.w):
+            for y in range(self.h):
+                rho = self.getRho(x, y)
+                phi = self.getPhi(x, y)
+                self.aMask[x, y] = math.sin(self.freq*phi + self.curv*rho)
+
+class StarFishImageMask(PolarImageMask):
+    """A starfish-like image."""
+    def __init__(self, w, h):
+        super().__init__('StarFishImageMask', w, h)
+        self.nBranches = 5
+        self.mu = 0.5
+
+    def randomize(self):
+        super().randomize()
+        self.nBranches = random.randrange(5, 9)
+        self.mu = random.random()
+
+    def runSimulation(self):
+        for x in range(self.w):
+            for y in range(self.h):
+                rho = self.getRho(x, y)
+                phi = self.getPhi(x, y)
+                self.aMask[x, y] = math.sin(self.nBranches*phi) * ImageMask.gauss(rho, self.mu, 0.5)
