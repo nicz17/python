d65adec90889477c799d8e1410c5efa5d65eebd0
nicz17 <nicolas.zwahlen@gmx.net>
2023-02-15 18:45:22 +0100

    Use FractalSet superclass

--- Fractals/FractalsApp.py
+++ Fractals/FractalsApp.py
@@ -8,10 +8,8 @@ __version__ = "1.0.0"
 
 import tkinter as tk
 import logging
-from PIL import Image
-import numpy as np
 from BaseApp import *
-from MandelbrotSet import *
+from FractalSet import *
 from Palette import *
 from Timer import *
 
@@ -20,13 +18,13 @@ class FractalsApp(BaseApp):
 
     def __init__(self, sTitle, sGeometry = '1200x700') -> None:
         self.iSize = 600
-        self.width = 3.0
         self.iMaxIter = 200
         #self.oFractal = MandelbrotSet(self.iMaxIter, 2.0)
         #self.oFractal = JuliaSet(self.iMaxIter, 2.0)
         self.oFractal = BurningShip(self.iMaxIter, 2.0)
         self.oPalette = HeatPalette()
         self.center = self.oFractal.getDefaultCenter()
+        self.width  = self.oFractal.getDefaultWidth()
         super().__init__(sTitle, sGeometry)
 
     def plot(self):
@@ -37,30 +35,21 @@ class FractalsApp(BaseApp):
         self.oImgPal = tk.PhotoImage(file = "images/palette.png")
         self.canPalette.create_image(self.iSize - 50, 25, anchor=tk.CENTER, image=self.oImgPal)
 
-        # try to draw live
+        # Draw fractal on canvas live
         self.oImgFract = tk.PhotoImage(width=self.iSize, height=self.iSize)
         self.canFractal.create_image(0, 0, anchor=tk.NW, image=self.oImgFract)
         timer = Timer()
         dx = self.width/self.iSize
         bl = self.center - complex(self.width/2.0, self.width/2.0)
-        rgbArray = np.zeros((self.iSize, self.iSize, 3), 'uint8')
         for x in range(self.iSize):
             for y in range(self.iSize):
                 c = bl + complex(x*dx, y*dx)
                 iter = self.oFractal.iter(c)
-                color = self.oPalette.getColor(iter/self.iMaxIter)
-                sColor = '#%02x%02x%02x' % (color[0], color[1], color[2])
-                #rgbArray[y, x, :] = color
+                sColor = self.oPalette.getColorHex(iter/self.iMaxIter)
                 self.oImgFract.put(sColor, (x, y))
-                #self.image.putpixel((x, size-1-y), color)
             if (x % 20 == 0):
                 self.window.update()
-        #self.updateDisplay()
 
-        #img = Image.fromarray(rgbArray)
-        #img.save('images/fractal.png', 'PNG')
-        #self.oImgFract = tk.PhotoImage(file = "images/fractal.png")
-        #self.canFractal.create_image(0, 0, anchor=tk.NW, image=self.oImgFract)
         self.window.configure(cursor='')
         self.setStatus('Plotted ' + self.oFractal.__str__() + ' in ' + timer.getElapsed())
     
@@ -76,7 +65,7 @@ class FractalsApp(BaseApp):
     def reset(self):
         self.setStatus('Resetting ' + self.oFractal.__str__())
         self.center = self.oFractal.getDefaultCenter()
-        self.width = 3.0
+        self.width  = self.oFractal.getDefaultWidth()
 
     def createWidgets(self):
         """Create user widgets"""
