8c379f11d7fc24202f3c74ba63d108e856a45c62
nicz17 <nicolas.zwahlen@gmx.net>
2023-02-15 18:44:42 +0100

    Add FractalSet superclass

--- Fractals/FractalSet.py
+++ Fractals/FractalSet.py
@@ -1,5 +1,6 @@
 """
- The Mandelbrot set fractal.
+ Various factal sets:
+ Mandelbrot, Julia, Burning Ship etc.
 """
 
 __author__ = "Nicolas Zwahlen"
@@ -8,14 +9,36 @@ __version__ = "1.0.0"
 
 import logging
 
-class MandelbrotSet:
+class FractalSet:
+    """A fractal set interface."""
+    log = logging.getLogger('FractalSet')
+
+    def __init__(self, sName, iMaxIter, rBailout = 2.0):
+        self.log.info('%s with %d iterations and %.1f bailout', sName, iMaxIter, rBailout)
+        self.sName = sName
+        self.iMaxIter = iMaxIter
+        self.rBailout = rBailout
+
+    def iter(self, z):
+        """Compute the iterations required to bailout at complex number z."""
+        return self.iMaxIter
+    
+    def getDefaultCenter(self) -> complex:
+        return complex(0.0, 0.0)
+    
+    def getDefaultWidth(self) -> float:
+        return 3.0
+    
+    def __str__(self):
+        return self.sName + ' with ' + str(self.iMaxIter) + ' iterations'
+    
+
+class MandelbrotSet(FractalSet):
     """The Mandelbrot set fractal."""
     log = logging.getLogger('MandelbrotSet')
 
     def __init__(self, iMaxIter, rBailout):
-        self.log.info('Mandelbrot set with %d iterations and %.1f bailout', iMaxIter, rBailout)
-        self.iMaxIter = iMaxIter
-        self.rBailout = rBailout
+        super().__init__('Mandelbrot set', iMaxIter, rBailout)
 
     def iter(self, z):
         """Compute the iterations required to bailout at complex number z."""
@@ -29,19 +52,13 @@ class MandelbrotSet:
     def getDefaultCenter(self):
         return complex(-0.75, 0.0)
     
-    def __str__(self):
-        return 'Mandelbrot set with ' + str(self.iMaxIter) + ' iterations'
-    
-    
 
-class JuliaSet:
+class JuliaSet(FractalSet):
     """The Julia set fractal."""
     log = logging.getLogger('JuliaSet')
 
     def __init__(self, iMaxIter, rBailout):
-        self.log.info('Julia set with %d iterations and %.1f bailout', iMaxIter, rBailout)
-        self.iMaxIter = iMaxIter
-        self.rBailout = rBailout
+        super().__init__('Julia set', iMaxIter, rBailout)
         self.c = complex(-0.73756, -0.16869)
 
     def iter(self, z):
@@ -52,21 +69,13 @@ class JuliaSet:
                 return i
         return self.iMaxIter
     
-    def getDefaultCenter(self):
-        return complex(0.0, 0.0)
-    
-    def __str__(self):
-        return 'Julia set with ' + str(self.iMaxIter) + ' iterations'
 
-
-class BurningShip:
+class BurningShip(FractalSet):
     """The Burning Ship fractal."""
     log = logging.getLogger('BurningShip')
 
     def __init__(self, iMaxIter, rBailout):
-        self.log.info('Burning Ship with %d iterations and %.1f bailout', iMaxIter, rBailout)
-        self.iMaxIter = iMaxIter
-        self.rBailout = rBailout
+        super().__init__('Burning Ship', iMaxIter, rBailout)
 
     def iter(self, z):
         """Compute the iterations required to bailout at complex number z."""
@@ -79,7 +88,7 @@ class BurningShip:
         return self.iMaxIter
     
     def getDefaultCenter(self):
-        return complex(0.0, 0.0)
+        return complex(0.0, -0.5)
     
-    def __str__(self):
-        return 'Burning Ship with ' + str(self.iMaxIter) + ' iterations'
+    def getDefaultWidth(self) -> float:
+        return 4.0
