5bc47eae5bc2dad0667b765b7867de96a1f88087
nicz17 <nicolas.zwahlen@gmx.net>
2023-04-12 20:18:58 +0200

    Test evolution

--- Qombo/test_qombit.py
+++ Qombo/test_qombit.py
@@ -13,26 +13,36 @@ class TestQombit(unittest.TestCase):
         self.star9 = QombitFactory.fromValues('Star', OrKind.Star, 9, OrRarity.Common)
         self.obj   = QombitFactory.createObjective(1, 1)
 
-    def test_generate(self):
+    def test_canGenerate(self):
         """Test qombit generation."""
         self.assertTrue(self.gen.canGenerate(), 'Expect generator can generate')
         self.assertFalse(self.basic.canGenerate(), 'Expect basic cannot generate')
         self.assertTrue(self.star9.canGenerate(), 'Expect lvl9 can generate')
         self.assertFalse(self.obj.canGenerate(), 'Expect objective cannot generate')
 
-    def test_sell(self):
+    def test_canSell(self):
         """Test qombit selling."""
         self.assertFalse(self.gen.canSell(), 'Expect generator cannot be sold')
         self.assertTrue(self.basic.canSell(), 'Expect basic can be sold')
         self.assertFalse(self.star9.canSell(), 'Expect lvl9 cannot be sold')
         self.assertFalse(self.obj.canSell(), 'Expect objective cannot be sold')
 
-    def test_evolve(self):
+    def test_canEvolve(self):
         """Test qombit evolution."""
         self.assertTrue(self.gen.canEvolve(), 'Expect generator can evolve')
         self.assertTrue(self.basic.canEvolve(), 'Expect basic can evolve')
         self.assertFalse(self.star9.canEvolve(), 'Expect lvl9 cannot evolve')
         self.assertFalse(self.obj.canEvolve(), 'Expect objective cannot evolve')
+
+    def test_evolve(self):
+        """Test qombit evolution."""
+        lvl1 = QombitFactory.fromValues('Star', OrKind.Star, 1, OrRarity.Common)
+        lvl2 = QombitFactory.fromValues('Star', OrKind.Star, 2, OrRarity.Common)
+        self.assertNotEqual(lvl1, lvl2, 'Expect lvl1 and lvl2 different')
+        self.assertTrue(lvl1.canEvolve(), 'Expect lvl1 can evolve')
+        lvl1.evolve()
+        #print(f'Level 1 evolved into {lvl1}')
+        self.assertEqual(lvl1, lvl2, 'Expect lvl1 evolved into lvl2')
 
     def test_json(self):
         """Test qombit to json and back."""
