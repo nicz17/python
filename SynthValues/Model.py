39f318ced370ccb6fcc751e4a9be9a75c5f0ff48
nicz17 <nicolas.zwahlen@gmx.net>
2023-05-10 21:39:57 +0200

    Generate synthetic values using a model

---
+++ SynthValues/Model.py
@@ -0,0 +1,78 @@
+"""
+A model to generate synthetic values.
+"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2023 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+import numpy  as np
+import pandas as pd
+
+class Model:
+    """Interface for generating synthetic values."""
+    log = logging.getLogger(__name__)
+
+    def __init__(self, name: str, nValues: int, dOptions: dict) -> None:
+        self.name = name
+        self.nValues = nValues
+        self.dOptions = dOptions
+        self.df = None
+        self.log.info('Model %s', name)
+
+    def createDataframe(self):
+        """
+        Create a Pandas Dataframe and fill it with synthetic values.
+        """
+        self.log.info('Generating DataFrame with %d %s values', self.nValues, self.name)
+        self.df = pd.DataFrame({
+                "day": np.arange(1, self.nValues+1),
+                "value": self.generate()
+            },
+            index = self.buildIndex()
+            )
+        self.df.index.name = 'index'
+        if self.dOptions['verbose']:
+            self.log.info('Dataframe:\n%s', self.df)
+
+    def buildIndex(self):
+        """Generate the dataframe index."""
+        return pd.date_range(start='01/01/2023', periods = self.nValues)
+
+    def generate(self):
+        """Generate the specified number of values."""
+        pass
+
+    def noise(self, ampl: float):
+        """Generate a random noise array"""
+        return np.random.uniform(low=-ampl, high=ampl, size=(self.nValues,))
+
+    def saveAsCSV(self):
+        """Save the DataFrame to a CSV file."""
+        sFilename = f'{self.name}.csv'
+        self.log.info('Saving synthetic data to %s', sFilename)
+        self.df.to_csv(sFilename)
+
+class RandomModel(Model):
+    """A model generating random values."""
+
+    def __init__(self, nValues: int, dOptions: dict) -> None:
+        super().__init__('RandomModel', nValues, dOptions)
+
+    def generate(self):
+        self.log.info('Generating %d %s values', self.nValues, self.name)
+        return np.random.rand(self.nValues)
+    
+class ConstantModel(Model):
+    """A model generating constant values with random noise."""
+
+    def __init__(self, nValues: int, value: float, dOptions: dict) -> None:
+        super().__init__('ConstantModel', nValues, dOptions)
+        self.value = value
+
+    def generate(self):
+        self.log.info('Generating %d %s values', self.nValues, self.name)
+        arrConst = np.repeat(self.value, self.nValues)
+        arrNoise = self.noise(0.9)
+        return np.add(arrConst, arrNoise)
---
+++ SynthValues/synthValuesGen.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+Generate synthetic values in Pandas dataframes,
+according to some model.
+Save the data to a CSV file.
+"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2023 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+import getopt
+import sys
+from Model import *
+
+
+def configureLogging():
+    """
+    Configures logging to have timestamped logs at INFO level on stdout.
+    """
+    
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+        level=logging.INFO,
+        datefmt = '%Y.%m.%d %H:%M:%S',
+        handlers=[logging.StreamHandler()])
+    return logging.getLogger('SynthValuesGen')
+
+def getOptions():
+    """Parse program arguments and store them in a dict."""
+
+    dOptions = {'verbose': False}
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hv', ['help', 'verbose'])
+    except getopt.GetoptError:
+        print("Invalid options: %s", sys.argv[1:])
+    for opt, arg in opts:
+        log.info("Parsing option %s value %s", opt, arg)
+        if opt in ('-h', '--help'):
+            print('synthValuesGen.py -h (help) -v (verbose)')
+            sys.exit()
+        elif opt in ("-v", "--verbose"):
+            dOptions['verbose'] = True
+    return dOptions
+
+def main():
+    """Main routine."""
+    log.info('Welcome to synthValuesGen v' + __version__)
+    #model = RandomModel(30, dOptions)
+    model = ConstantModel(30, 100.0, dOptions)
+    model.createDataframe()
+    model.saveAsCSV()
+
+log = configureLogging()
+dOptions = getOptions()
+main()
