0abba6a2e7766ec9ad9ae8065169b0c4c633fcd4
Nicolas Zwahlen <nicolas.zwahlen@gmx.net>
2023-05-30 16:33:02 +0200

    Add draft Mesh class

---
+++ Orfact/Mesh.py
@@ -0,0 +1,119 @@
+"""
+A 2D mesh generator.
+The mesh is made of vertices connected by edges.
+Three vertices and their edges make a face.
+"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2023 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+import math
+
+class Vertex:
+    """A 2D vertex for a mesh."""
+    log = logging.getLogger('Vertex')
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x 
+        self.y = y
+
+    def dist(self, v):
+        dx = self.x - v.x
+        dy = self.y - v.y
+        return math.sqrt(dx*dx + dy*dy)
+        
+    def __eq__(self, other): 
+        if not isinstance(other, Vertex):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __str__(self):
+        return f'Vertex {self.x}:{self.y}'
+
+class Edge:
+    """An edge connecting two vertices."""
+    log = logging.getLogger('Edge')
+
+    def __init__(self, v1: Vertex, v2: Vertex) -> None:
+        self.v1 = v1
+        self.v2 = v2
+    
+    def __eq__(self, other): 
+        if not isinstance(other, Edge):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return (self.v1 == other.v1 and self.v2 == other.v2) or (self.v1 == other.v2 and self.v2 == other.v1)
+
+    def __hash__(self):
+        return hash((self.v1, self.v2)) + hash((self.v2, self.v1))
+
+    def __str__(self):
+        return f'Edge from {self.v1} to {self.v2}'
+
+
+class Mesh:
+    """A mesh with vertices and edges."""
+    log = logging.getLogger('Mesh')
+
+    def __init__(self) -> None:
+        self.log.info('Mesh constructor')
+        self.vertices = []
+        self.edges = set()
+
+    def addVertex(self, v: Vertex):
+        self.vertices.append(v)
+
+    def buildEdges(self):
+        self.log.info('Building edges')
+        for v in self.vertices:
+            v2 = self.getClosest(v)
+            v3 = self.getClosest(v, v2)
+            if v2 is not None:
+                self.edges.add(Edge(v, v2))
+            if v3 is not None:
+                self.edges.add(Edge(v, v3))
+                if len(self.edges) < 3:
+                    self.edges.add(Edge(v2, v3))
+
+    def getClosest(self, v: Vertex, exc=None):
+        result = None
+        minDist = None
+        for vertex in self.vertices:
+            if v == vertex or exc == vertex:
+                continue
+            dist = v.dist(vertex)
+            if minDist is None or dist < minDist:
+                minDist = dist
+                result = vertex
+        return result
+
+    def dump(self):
+        self.log.info(self)
+        for vertex in self.vertices:
+            self.log.info('  %s', vertex)
+        for edge in self.edges:
+            self.log.info('  %s', edge)
+
+    def __str__(self):
+        return f'Mesh with {len(self.vertices)} vertices and {len(self.edges)} edges'
+
+def demoMesh():
+    """Simple test and demo method for the mesh class."""
+    mesh = Mesh()
+    mesh.addVertex(Vertex(0.0, 1.0))
+    mesh.addVertex(Vertex(1.0, 0.0))
+    mesh.addVertex(Vertex(1.0, 2.0))
+    mesh.addVertex(Vertex(2.1, 1.0))
+    mesh.buildEdges()
+    mesh.dump()
+
+if __name__ == '__main__':
+    logging.basicConfig(format="[%(levelname)s] %(message)s", 
+        level=logging.DEBUG, handlers=[logging.StreamHandler()])
+    demoMesh()
