14f62d79c5301d6d0ddcebf78563ed582b5cef44
Nicolas Zwahlen <nicolas.zwahlen@gmx.net>
2023-06-29 15:43:27 +0200

    Add toJson

--- Common/Graph.py
+++ Common/Graph.py
@@ -12,42 +12,43 @@ class Node:
     """A Node for a Graph."""
     log = logging.getLogger('Node')
 
-    def __init__(self, name: str) -> None:
-        """Constructor with name of the Node."""
+    def __init__(self, id: int, name: str) -> None:
+        """Constructor with ID and name of the Node."""
+        self.id = id
         self.name = name
         
     def __eq__(self, other): 
         if not isinstance(other, Node):
             # don't attempt to compare against unrelated types
             return NotImplemented
-        return self.name == other.name
+        return self.id == other.id
 
     def __hash__(self):
         return hash((self.name))
 
     def __str__(self):
-        return f'Node {self.name}'
+        return f'Node {self.id} {self.name}'
 
 class Edge:
     """An edge connecting two nodes."""
     log = logging.getLogger('Edge')
 
-    def __init__(self, v1: Node, v2: Node) -> None:
+    def __init__(self, n1: Node, n2: Node) -> None:
         """Constructor with two nodes."""
-        self.v1 = v1
-        self.v2 = v2
+        self.n1 = n1
+        self.n2 = n2
     
     def __eq__(self, other): 
         if not isinstance(other, Edge):
             # don't attempt to compare against unrelated types
             return NotImplemented
-        return (self.v1 == other.v1 and self.v2 == other.v2) or (self.v1 == other.v2 and self.v2 == other.v1)
+        return (self.n1 == other.n1 and self.n2 == other.n2) or (self.n1 == other.n2 and self.n2 == other.n1)
 
     def __hash__(self):
-        return hash((self.v1, self.v2)) + hash((self.v2, self.v1))
+        return hash((self.n1, self.n2)) + hash((self.n2, self.n1))
 
     def __str__(self):
-        return f'Edge from {self.v1} to {self.v2}'
+        return f'Edge from {self.n1} to {self.n2}'
 
 
 class Graph:
@@ -59,9 +60,9 @@ class Graph:
         self.nodes = []
         self.edges = set()
 
-    def addNode(self, v: Node):
+    def addNode(self, node: Node):
         """Add a Node to this Graph."""
-        self.nodes.append(v)
+        self.nodes.append(node)
 
     def addEdge(self, edge: Edge):
         """Add an Edge to this graph."""
@@ -70,10 +71,19 @@ class Graph:
     def dump(self):
         """Dump the Graph details to log."""
         self.log.info(self)
-        for Node in self.nodes:
-            self.log.info('  %s', Node)
+        for node in self.nodes:
+            self.log.info('  %s', node)
         for edge in self.edges:
             self.log.info('  %s', edge)
+
+    def toJson(self):
+        data = []
+        for edge in self.edges:
+            dataEdge = []
+            dataEdge.append(edge.n1.name)
+            dataEdge.append(edge.n2.name)
+            data.append(dataEdge)
+        return data
 
     def __str__(self):
         return f'Graph with {len(self.nodes)} nodes and {len(self.edges)} edges'
@@ -81,9 +91,9 @@ class Graph:
 def demoGraph():
     """Simple test and demo method for the Graph class."""
     graph = Graph()
-    nodeA = Node('A')
-    nodeB = Node('B')
-    nodeC = Node('C')
+    nodeA = Node(1, 'A')
+    nodeB = Node(2, 'B')
+    nodeC = Node(3, 'C')
     graph.addNode(nodeA)
     graph.addNode(nodeB)
     graph.addNode(nodeC)
@@ -91,6 +101,7 @@ def demoGraph():
     graph.addEdge(Edge(nodeB, nodeC))
     graph.addEdge(Edge(nodeC, nodeA))
     graph.dump()
+    graph.log.info('JSON: %s', graph.toJson())
 
 if __name__ == '__main__':
     logging.basicConfig(format="[%(levelname)s] %(message)s", 
