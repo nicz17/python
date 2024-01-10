3bab2819456143bf18206a428668f916228f5dd9
nicz17 <nicolas.zwahlen@gmx.net>
2024-01-10 21:45:34 +0100

    Add color circles for tasks

--- Pynorpa/Renderer.py
+++ Pynorpa/Renderer.py
@@ -18,6 +18,7 @@ class Renderer:
     log = logging.getLogger('Renderer')
 
     def __init__(self, canvas: tk.Canvas, root: tk.Tk):
+        """Constructor. Initialise sizes and fonts."""
         self.fontBold = tkfont.Font(family="Helvetica", size=12, weight='bold')
         self.log.info('Constructor')
         self.canvas = canvas
@@ -26,6 +27,7 @@ class Renderer:
 
     def drawTasks(self, tasks):
         """Draw the specified tasks on the canvas."""
+        self.canvas.delete('all')
         iTask = 0
         for task in tasks:
             self.drawTask(task, iTask)
@@ -35,6 +37,14 @@ class Renderer:
         """Draw the specified task on the canvas."""
         sx = 10
         sy = iTask*100 + 10
+        colorTask = 'red'
+        if task.isOver():
+            colorTask = 'green'
         self.canvas.create_rectangle(sx, sy, sx+self.width, sy+90, outline = self.colorGridLines)
-        self.canvas.create_text(sx+5, sy+15, text = task.title, anchor = 'w', font = self.fontBold)
-        self.canvas.create_text(sx+5, sy+35, text = task.desc,  anchor = 'w')
+        self.canvas.create_oval(sx+5,  sy+15, sx+40, sy+50, fill=colorTask)
+        self.canvas.create_text(sx+55, sy+15, text = task.title, anchor = 'w', font = self.fontBold)
+        self.canvas.create_text(sx+55, sy+35, text = task.desc,  anchor = 'w')
+        self.canvas.create_text(sx+55, sy+55, text = task.getStatus(), anchor = 'w')
+
+    def __str__(self):
+        return 'Renderer for Pynorpa tasks'
