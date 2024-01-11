454e61829f7db6f8b1745fdc99a52dc624088a9b
nicz17 <nicolas.zwahlen@gmx.net>
2024-01-11 22:52:29 +0100

    Use exifread to get all EXIF tags

--- Pynorpa/PhotoInfo.py
+++ Pynorpa/PhotoInfo.py
@@ -10,7 +10,6 @@ __version__ = "1.0.0"
 import logging
 import glob
 import exifread
-from PIL import Image, ExifTags
 
 class PhotoInfo:
     log = logging.getLogger(__name__)
@@ -22,34 +21,26 @@ class PhotoInfo:
         self.shotat = None
         self.width  = None
         self.height = None
+        self.lat = None
+        self.lon = None
 
     def identify(self):
         """Find details like size and shot-at about this image file."""
-        img = Image.open(self.filename)
-        self.width, self.height = img.size
-
-        # Read EXIF data
-        exif = img.getexif()
-        if exif is None:
-            self.log.error('Image has no EXIF data.')
-        else:
-            for key, val in exif.items():
-                if key in ExifTags.TAGS and ExifTags.TAGS[key] == 'DateTime':
-                    self.shotat = val
+        file = open(self.filename, 'rb')
+        tags = exifread.process_file(file)
+        for tag in tags.keys():
+            if tag == 'EXIF DateTimeOriginal':
+                self.shotat = tags[tag]
+            elif tag == 'EXIF ExifImageWidth':
+                self.width = tags[tag]
+            elif tag == 'EXIF ExifImageLength':
+                self.height = tags[tag]
+        self.lat, self.lon = exifread.utils.get_gps_coords(tags)
+        file.close()
+        self.log.info(self)
 
     def listAllExif(self):
         """List all existing EXIF tags."""
-        img = Image.open(self.filename)
-        exif = img.getexif()
-        if exif is None:
-            self.log.error('Image has no EXIF data.')
-        else:
-            self.log.info('Found %d EXIF tags:', len(exif.items()))
-            for key, val in exif.items():
-                if key in ExifTags.TAGS:
-                    self.log.info('Tag %s : %s', ExifTags.TAGS[key], val)
-
-    def listExifread(self):
         file = open(self.filename, 'rb')
         tags = exifread.process_file(file)
         for tag in tags.keys():
@@ -57,9 +48,10 @@ class PhotoInfo:
                 self.log.info('Tag %s : %s', tag, tags[tag])
             else:
                 self.log.info('Skipping tag %s', tag)
+        file.close()
                     
     def __str__(self):
-        str = f'PhotoInfo {self.filename} [{self.width}x{self.height}] {self.shotat}'
+        str = f'PhotoInfo {self.filename} [{self.width}x{self.height}] {self.shotat} Lon/Lat {self.lon}/{self.lat}'
         return str
     
 def testPhotoInfo():
@@ -68,7 +60,7 @@ def testPhotoInfo():
     for img in images:
         info = PhotoInfo(img)
         #info.listAllExif()
-        info.listExifread()
+        info.identify()
 
 if __name__ == '__main__':
     logging.basicConfig(format="[%(levelname)s] %(message)s", 
