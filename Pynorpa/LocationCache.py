fe2ad53b82773c5b16dfa2a48429ed69f80cd0cb
nicz17 <nicolas.zwahlen@gmx.net>
2024-01-19 21:46:51 +0100

    Add Location class, find closest

--- Pynorpa/LocationCache.py
+++ Pynorpa/LocationCache.py
@@ -8,8 +8,30 @@ __version__ = "1.0.0"
 
 import config
 import logging
+import math
 from Database import *
 
+
+class Location:
+    """Container for Panorpa location object."""
+    log = logging.getLogger('LocationCache')
+
+    def __init__(self, row):
+        """Constructor from fetched row."""
+        self.idx = row[0]
+        self.name = row[1]
+        self.desc = row[2]
+        self.lat = row[3]
+        self.lon = row[4]
+
+    def getDistance(self, lat: float, lon: float) -> float:
+        """Get the distance to another location."""
+        return math.sqrt((self.lat - lat) ** 2 + (self.lon - lon) ** 2)
+
+    def __str__(self):
+        str = f'Location {self.idx} {self.name}'
+        return str
+    
 
 class LocationCache:
     """Fetch and store Location records from Panorpa database."""
@@ -27,14 +49,38 @@ class LocationCache:
         sql = 'select idxLocation, locName, locDesc, locLatitude, locLongitude from Location'
         rows = db.fetch(sql)
         for row in rows:
-            self.log.info(row)
+            self.log.debug(row)
+            loc = Location(row)
+            self.locations.append(loc)
+            self.log.debug(loc)
         db.disconnect()
+        self.log.info('Done loading %s', self)
+
+    def getClosest(self, lat: float, lon: float) -> Location:
+        """Find the closest location in this cache to the specified lat/lon."""
+        closest = None
+        minDist = 100000
+        for loc in self.locations:
+            dist = loc.getDistance(lat, lon)
+            if dist < minDist:
+                closest = loc
+                minDist = dist
+        return closest
+
+    def __str__(self):
+        str = f'LocationCache with {len(self.locations)} locations'
+        return str
 
 
 def testLocationCache():
     """Simple test case for LocationCache class."""
     cache = LocationCache()
     cache.load()
+    closest = cache.getClosest(46.52219597, 6.57062216)
+    print(closest)
+    closest = cache.getClosest(46.6011348, 6.7310774)
+    print(closest)
+
 
 if __name__ == '__main__':
     logging.basicConfig(format="%(levelname)s %(name)s: %(message)s", 
