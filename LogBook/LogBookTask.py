68799e35dc6e8f32af2e5b777f257524c1887c42
nicz17 <nicolas.zwahlen@gmx.net>
2024-02-16 18:03:53 +0100

    Add LogBookStep class

--- LogBook/LogBookTask.py
+++ LogBook/LogBookTask.py
@@ -9,6 +9,46 @@ __version__ = "1.0.0"
 import logging
 import time
 import DateTools
+from enum import Enum
+
+class Status(Enum):
+    Idle = 0
+    Done = 1
+    Cancelled = 2
+
+    def __str__(self):
+        return self.name
+    
+class LogBookStep:
+    """A step in a LogBook task."""
+    log = logging.getLogger(__name__)
+
+    def __init__(self, text: str, status: Status):
+        """Constructor with text, status."""
+        self.text = text
+        if status is None:
+            status = Status.Idle
+        self.status = status
+
+    def toJson(self):
+        """Export this step as JSON."""
+        data = {
+            'text': self.text,
+            'status': self.status.name
+        }
+        return data
+    
+    @staticmethod
+    def fromJson(data):
+        """Load data from a JSON object."""
+        text = data.get('text')
+        status = Status[data.get('status')]
+        return LogBookStep(text, status)
+    
+    def __str__(self):
+        str = f'LogBookStep {self.text} {self.status}'
+        return str
+
 
 class LogBookTask:
     """A task with steps for the LogBook app."""
@@ -22,12 +62,20 @@ class LogBookTask:
         self.created = created
         self.steps = steps
 
+    def addStep(self, step: LogBookStep):
+        """Add a step to this task."""
+        if step is not None:
+            self.steps.append(step)
+
     def toJson(self):
         """Export this task as JSON."""
+        dataSteps = []
+        for step in self.steps:
+            dataSteps.append(step.toJson())
         data = {
             'title': self.title,
             'created': DateTools.timestampToString(self.created),
-            'steps': self.steps
+            'steps': dataSteps
         }
         return data
     
@@ -36,7 +84,10 @@ class LogBookTask:
         """Load data from a JSON object."""
         title = data.get('title')
         created = DateTools.stringToTimestamp(data.get('created'))
-        steps = data.get('steps')
+        #steps = data.get('steps')
+        steps = []
+        for dataStep in data.get('steps'):
+            steps.append(LogBookStep.fromJson(dataStep))
         return LogBookTask(title, steps, created)
     
     def __str__(self):
