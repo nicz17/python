b39c4d688a31b2a23f350aac42121ea0042057f4
nicz17 <nicolas.zwahlen@gmx.net>
2024-03-03 21:21:01 +0100

    Add python subclass

--- SimpleUML/SimpleUMLClass.py
+++ SimpleUML/SimpleUMLClass.py
@@ -9,12 +9,77 @@ __version__ = "1.0.0"
 import logging
 import os
 import DateTools
+from CodeFile import *
 
 
 class SimpleUMLClass():
-    """A simple class."""
+    """A simple code generator."""
     log = logging.getLogger('SimpleUMLClass')
+    dir = 'test'
 
     def __init__(self) -> None:
         """Constructor."""
         self.log.info('Constructor')
+        self.name = None
+        self.members = []
+        self.methods = []
+
+    def setName(self, name: str):
+        """Define the class name."""
+        self.name = name
+
+    def addMember(self, name: str, type: str):
+        """Add a class member of the specified name and type."""
+        self.log.info('Adding class member %s type %s', name, type)
+        self.members.append(name)
+
+    def generate(self):
+        """Generate the code."""
+        pass
+
+class SimpleUMLClassPython(SimpleUMLClass):
+    """A simple python code generator."""
+    log = logging.getLogger('SimpleUMLClassPython')
+
+    def __init__(self) -> None:
+        """Constructor."""
+        super().__init__()
+
+    def generate(self):
+        """Generate the code."""
+        filename = f'{self.dir}/{self.name}.py'
+        self.log.info('Generating %s', filename)
+        file = CodeFile(filename)
+
+        # Header
+        file.comment(f'Module {self.name}')
+        file.newline()
+        file.write('__author__ = "Nicolas Zwahlen"')
+        file.write('__copyright__ = "Copyright 2024 N. Zwahlen"')
+        file.write('__version__ = "1.0.0"')
+        file.newline()
+
+        # Imports
+        file.write('import logging')
+        file.newline(2)
+
+        # Class declaration
+        file.write(f'class {self.name}():')
+        file.comment(f'Class {self.name}', 1)
+        file.write(f'log = logging.getLogger("{self.name}")', 1)
+        file.newline()
+
+        # Constructor
+        file.write('def __init__(self):', 1)
+        file.comment('Constructor.', 2)
+        for member in self.members:
+            file.write(f'self.{member} = None', 2)
+        file.newline()
+
+        # toString method
+        file.write('def __str__(self):', 1)
+        file.write(f'str = "{self.name}"', 2)
+        file.write('return str', 2)
+        file.newline()
+
+        file.close()
