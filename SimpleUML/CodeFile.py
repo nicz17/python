ce41114fc81e5a179afc3490fe920824ee42fe07
nicz17 <nicolas.zwahlen@gmx.net>
2024-03-03 21:20:44 +0100

    File wrapper to generate code

---
+++ SimpleUML/CodeFile.py
@@ -0,0 +1,31 @@
+"""
+Write code to a file.
+"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2024 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+
+class CodeFile():
+    """Write code to a file."""
+    log = logging.getLogger('CodeFile')
+
+    def __init__(self, filename: str) -> None:
+        """Constructor."""
+        self.log.info('Constructor')
+        self.file = open(filename, 'w')
+
+    def comment(self, text: str, indent = 0):
+        self.write(f'"""{text}"""', indent)
+
+    def newline(self, amount = 1):
+        self.file.write('\n' * amount)
+
+    def write(self, line: str, indent = 0):
+        self.file.write('\t' * indent + line + '\n')
+
+    def close(self):
+        """Close our file."""
+        self.file.close()
