be30043f641e00db3a79aeb516a9026ff023938e
nicz17 <nicolas.zwahlen@gmx.net>
2024-03-29 17:22:17 +0100

    Add progress bar with color

--- Pynorpa/Renderer.py
+++ Pynorpa/Renderer.py
@@ -11,6 +11,7 @@ from tkinter import font as tkfont
 import logging
 from BaseApp import *
 from Task import *
+from Palette import GreenRedPalette
 
 class Renderer:
     """Rendering methods for Pynorpa."""
@@ -21,6 +22,7 @@ class Renderer:
         """Constructor. Initialise sizes and fonts."""
         self.fontBold = tkfont.Font(family="Helvetica", size=12, weight='bold')
         self.log.info('Constructor')
+        self.palette = GreenRedPalette()
         self.canvas = canvas
         self.root = root
         self.width = 750
@@ -36,20 +38,27 @@ class Renderer:
     def drawTask(self, task: Task, iTask: int):
         """Draw the specified task on the canvas."""
         sx = 10
-        sy = iTask*100 + 10
-        self.canvas.create_rectangle(sx, sy, sx+self.width, sy+90, outline = self.colorGridLines)
+        sy = iTask*105 + 10
+        self.canvas.create_rectangle(sx, sy, sx+self.width, sy+96, outline = self.colorGridLines)
         self.canvas.create_oval(sx+8,  sy+15, sx+43, sy+50, fill=self.getTaskColor(task), outline="")
         self.canvas.create_text(sx+55, sy+15, text=task.title, anchor='w', font=self.fontBold)
         self.canvas.create_text(sx+55, sy+35, text=task.desc,  anchor='w')
         self.canvas.create_text(sx+55, sy+55, text=task.getStatus(), anchor='w')
+        self.canvas.create_rectangle(sx+55, sy+70, sx+self.width-20, sy+87, fill='#a0a0a0', outline='')
+        if task.nStepsTotal > 0 and task.nStepsDone > 0:
+            widthProg = int((self.width-75)*task.nStepsDone/task.nStepsTotal)
+            color = self.palette.getColorHex(1.0 - task.nStepsDone/task.nStepsTotal)
+            self.canvas.create_rectangle(sx+55, sy+70, sx+55+widthProg, sy+87, fill=color, outline='')
 
     def getTaskColor(self, task: Task) -> str:
         """Get the color of the task icon."""
-        color = '#ff4000'
-        if task.isOver():
-            color = '#40ff00'
+        color = '#ff4000'  # not started
+        if task.nStepsTotal == 0:
+            color = '#a0a0a0'  # undefined
+        elif task.isOver():
+            color = '#40ff00'  # done
         elif task.nStepsDone > 0:
-            color = '#ffa500'
+            color = '#ffa500'  # ongoing
         return color
 
     def __str__(self):
