07629349fc9faac395d6ca721fdbce29d8ff223f
nicz17 <nicolas.zwahlen@gmx.net>
2024-05-02 16:10:09 +0200

    Editor with grid layout

--- Pynorpa/LocationEditor.py
+++ Pynorpa/LocationEditor.py
@@ -26,8 +26,10 @@ class LocationEditor():
         """Display the specified object in this editor."""
         self.location = location
         self.txtName.delete(0, tk.END)
+        self.txtRegion.delete(0, tk.END)
         if location:
             self.txtName.insert(0, location.name)
+            self.txtRegion.insert(0, location.region)
 
     def onSave(self, evt = None):
         """Save changes to the edited object."""
@@ -37,7 +39,7 @@ class LocationEditor():
 
     def onCancel(self):
         """Cancel changes to the edited object."""
-        #self.loadData(self.location)
+        self.loadData(self.location)
 
     def createWidgets(self, parent: tk.Frame):
         """Add the editor widgets to the parent widget."""
@@ -45,19 +47,22 @@ class LocationEditor():
         frmEdit.pack(side=tk.TOP, anchor=tk.N, fill=tk.X, expand=True, pady=5)
 
         # Name
-        self.lblName = tk.Label(frmEdit, text='Name')
-        self.lblName.pack(anchor=tk.W)
-        self.txtName = ttk.Entry(frmEdit)
-        self.txtName.pack(anchor=tk.W, fill=tk.X)
+        self.lblName = tk.Label(frmEdit, text='Nom')
+        self.lblName.grid(row=0, column=0, sticky='w')
+        self.txtName = ttk.Entry(frmEdit, width=64)
+        self.txtName.grid(row=0, column=1, padx=5, sticky='ew')
 
-        # Buttons frame
+        # Region
+        self.lblRegion = tk.Label(frmEdit, text='Région')
+        self.lblRegion.grid(row=1, column=0, sticky='w')
+        self.txtRegion = ttk.Entry(frmEdit, width=64)
+        self.txtRegion.grid(row=1, column=1, padx=5, sticky='ew')
+
+
+        # Buttons: save, cancel
         frmButtons = ttk.Frame(frmEdit, padding=5)
-        frmButtons.pack()
-        
-        # Save button
+        frmButtons.grid(row=2, column=0, columnspan=2)
         self.btnSave = tk.Button(frmButtons, text = 'Save', command = self.onSave)
-        self.btnSave.pack(side=tk.LEFT)
-        
-        # Cancel button
+        self.btnSave.grid(row=0, column=0)
         self.btnCancel = tk.Button(frmButtons, text = 'Cancel', command = self.onCancel)
-        self.btnCancel.pack(side=tk.LEFT, padx=5)
+        self.btnCancel.grid(row=0, column=1, padx=5)
