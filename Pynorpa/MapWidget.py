e814bb1f76106611de9098652d566e885115ab9b
nicz17 <nicolas.zwahlen@gmx.net>
2024-05-11 13:55:34 +0200

    Add default location for map

--- Pynorpa/MapWidget.py
+++ Pynorpa/MapWidget.py
@@ -17,6 +17,11 @@ from LocationCache import *
 class MapWidget():
     """A map widget."""
     log = logging.getLogger('MapWidget')
+    locZero = {
+        'lat': 0.0,
+        'lon': 0.0,
+        'zoom': 15
+    }
 
     def __init__(self):
         """Constructor"""
@@ -24,11 +29,20 @@ class MapWidget():
 
     def loadData(self, location: Location):
         """Display the specified location on the map."""
-        self.mapView.set_position(location.lat, location.lon)
-        self.mapView.set_zoom(location.zoom)
+        if location is not None:
+            self.mapView.set_position(location.lat, location.lon)
+            self.mapView.set_zoom(location.zoom)
+        else:
+            self.setDefaultLocation()
+
+    def setDefaultLocation(self):
+        """Display the default location on the map."""
+        self.mapView.set_position(self.locZero['lat'], self.locZero['lon'])
+        self.mapView.set_zoom(self.locZero['zoom'])
         
     def createWidgets(self, parent: tk.Frame):
         """Create user widgets."""
         self.mapView = tkintermapview.TkinterMapView(parent, width=600, height=400, corner_radius=0)
         self.mapView.pack()
+        self.setDefaultLocation()
     
