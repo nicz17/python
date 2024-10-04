3128daa0f89b540b5f595e70c5d8df20da9e46ac
nicz17 <nicolas.zwahlen@gmx.net>
2024-10-04 16:46:23 +0200

    Add draft Taxon editor

--- Pynorpa/moduleTaxon.py
+++ Pynorpa/moduleTaxon.py
@@ -9,6 +9,7 @@ __version__ = "1.0.0"
 import logging
 from TabsApp import *
 from BaseTree import *
+import BaseWidgets
 from taxon import Taxon, TaxonCache
 
 
@@ -20,15 +21,15 @@ class ModuleTaxon(TabModule):
         """Constructor."""
         self.window = parent.window
         super().__init__(parent, 'Taxons')
-        self.cache = TaxonCache()
-        self.tree = TaxonTree(self.onSelectTaxon)
+        self.cache  = TaxonCache()
+        self.tree   = TaxonTree(self.onSelectTaxon)
+        self.editor = TaxonEditor(self.onSaveTaxon)
 
     def loadData(self):
         self.cache.load()
         tax: Taxon
         for tax in self.cache.getTopLevelTaxa():
             self.tree.addTaxon(tax)
-        #self.tree.addTestItems()
 
 
     def onSelectTaxon(self, id: str):
@@ -40,7 +41,10 @@ class ModuleTaxon(TabModule):
         self.log.info('Selected %s', taxon)
 
         # Display in widgets
-        #self.editor.loadData(taxon)
+        self.editor.loadData(taxon)
+
+    def onSaveTaxon(self, taxon: Taxon):
+        pass
 
     def createWidgets(self):
         """Create user widgets."""
@@ -53,7 +57,7 @@ class ModuleTaxon(TabModule):
 
         # Taxon tree and editor
         self.tree.createWidgets(self.frmLeft)
-        #self.editor.createWidgets(self.frmRight)
+        self.editor.createWidgets(self.frmRight)
 
 class TaxonTree(BaseTree):
     """Subclass of BaseTree displaying taxa."""
@@ -74,3 +78,76 @@ class TaxonTree(BaseTree):
 
     def onSelectEvent(self, event):
         self.cbkSelectItem(self.getSelectedId())
+
+class TaxonEditor(BaseWidgets.BaseEditor):
+    """A widget for editing Pynorpa taxa."""
+    log = logging.getLogger(__name__)
+
+    def __init__(self, cbkSave):
+        """Constructor with save callback."""
+        super().__init__(cbkSave)
+        self.taxon = None
+
+    def loadData(self, taxon: Taxon):
+        """Display the specified object in this editor."""
+        self.taxon = taxon
+        self.enableWidgets()
+        self.txtName.setValue(None)
+        self.txtNameFr.setValue(None)
+        if taxon:
+            self.txtName.setValue(taxon.name)
+            self.txtNameFr.setValue(taxon.nameFr)
+
+    def hasChanges(self) -> bool:
+        """Check if the editor has any changes."""
+        if self.taxon:
+            if self.taxon.name != self.txtName.getValue():
+                return True
+            if self.taxon.nameFr != self.txtNameFr.getValue():
+                return True
+        return False
+
+    def onSave(self, evt = None):
+        """Save changes to the edited object."""
+        self.cbkSave(self.taxon)
+
+    def onCancel(self):
+        """Cancel changes to the edited object."""
+        self.loadData(self.taxon)
+
+    def onDelete(self):
+        """Delete the edited object."""
+        pass
+
+    def createWidgets(self, parent: tk.Frame):
+        """Add the editor widgets to the parent widget."""
+        super().createWidgets(parent, 'Taxon Editor')
+
+        # Location attributes
+        self.txtName     = self.addText('Nom latin')
+        self.txtNameFr   = self.addText('Nom français')
+
+        # Buttons: save, cancel
+        frmButtons = ttk.Frame(self.frmEdit, padding=5)
+        frmButtons.grid(row=self.row, column=0, columnspan=2)
+        self.btnSave = tk.Button(frmButtons, text = 'Save', command = self.onSave)
+        self.btnSave.grid(row=0, column=0, padx=3)
+        self.btnCancel = tk.Button(frmButtons, text = 'Cancel', command = self.onCancel)
+        self.btnCancel.grid(row=0, column=1, padx=3)
+        self.btnDelete = tk.Button(frmButtons, text = 'Delete', command = self.onCancel)
+        self.btnDelete.grid(row=0, column=2, padx=3)
+
+        self.enableWidgets()
+
+    def enableWidgets(self, evt=None):
+        """Enable our internal widgets."""
+        editing  = self.taxon is not None
+        modified = self.hasChanges()
+        self.txtName.enableWidget(editing)
+        self.txtNameFr.enableWidget(editing)
+        BaseWidgets.enableWidget(self.btnSave, modified)
+        BaseWidgets.enableWidget(self.btnCancel, modified)
+        BaseWidgets.enableWidget(self.btnDelete, False)
+
+    def __str__(self) -> str:
+        return 'TaxonEditor'
