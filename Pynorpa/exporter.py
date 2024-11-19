52c9f65f39ced5a43cfe9be897965a7401e51a96
nicz17 <nicolas.zwahlen@gmx.net>
2024-11-19 20:40:33 +0100

    Add HTML page exporter draft

---
+++ Pynorpa/exporter.py
@@ -0,0 +1,79 @@
+"""Module Exporter"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2024 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+import pynorpaHtml
+from HtmlPage import *
+
+class Exporter():
+    """Class Exporter"""
+    log = logging.getLogger("Exporter")
+
+    def __init__(self):
+        """Constructor."""
+        self.cache = None
+
+    def buildHome(self):
+        """BuildHome"""
+        # TODO: implement
+        pass
+
+    def buildLinks(self):
+        """BuildLinks"""
+        # TODO: implement
+        pass
+
+    def buildLocations(self):
+        """BuildLocations"""
+        # TODO: implement
+        pass
+
+    def buildTaxa(self):
+        """BuildTaxa"""
+        # TODO: implement
+        pass
+
+    def buildTest(self):
+        """Build a simple test page."""
+        page = pynorpaHtml.PynorpaHtmlPage('Test')
+        page.addHeading(1, 'Test')
+        page.addHeading(2, 'Bibliographie')
+
+        biblio = []
+        self.addBiblioRef(biblio, "P. Leraut, P. Blanchot", "Le guide entomologique", "Delachaux et Niestlé", "2012")
+        self.addBiblioRef(biblio, "R. Dajoz", "Dictionnaire d'entomologie", "Lavoisier", "2010")
+        self.addBiblioRef(biblio, "D. Martiré", "Guide des plus beaux coléoptères", "Belin", "2017")
+        self.addBiblioRef(biblio, "K. Dijkstra", "Guide des libellules", "Delachaux et Niestlé", "2015")
+        self.addBiblioRef(biblio, "T. Haahtela <i>et al</i>", "Guide photo des papillons d'Europe", "Delachaux et Niestlé", "2017")
+        self.addBiblioRef(biblio, "R. Garrouste", "Hémiptères de France", "Delachaux et Niestlé", "2015")
+        self.addBiblioRef(biblio, "A. Canard, C. Rollard", "A la découverte des araignées", "Dunod", "2015")
+        self.addBiblioRef(biblio, "K. Lauber, G. Wagner, A. Gygax", "Flora Helvetica", "4e édition, Haupt", "2012")
+        self.addBiblioRef(biblio, "E. Gerber, G. Kozlowski, A.-S. Mariéthoz", "La flore des Préalpes", "Rossolis", "2010")
+        self.addBiblioRef(biblio, "F. Dupont, J.-L. Guignard", "Botanique, les familles de plantes", "15e édition, Elsevier Masson", "2012")
+        self.addBiblioRef(biblio, "Collectif", "Les guides Salamandre", "Editions de la Salamandre, Neuchâtel", None)
+        page.addList(biblio)
+
+        page.save('export/test.html')
+
+    def addBiblioRef(self, list, authors: str, title: str, editor: str, year: str):
+        """Add a bibliographical reference."""
+        nsyear = f', {year}' if year else '' 
+        list.append(f'{authors} : <b>{title}</b>, {editor}{nsyear}')
+
+    def __str__(self):
+        return 'Exporter'
+
+
+def testExporter():
+    """Unit test for Exporter"""
+    Exporter.log.info("Testing Exporter")
+    obj = Exporter()
+    obj.buildTest()
+
+if __name__ == '__main__':
+    logging.basicConfig(format="%(levelname)s %(name)s: %(message)s",
+        level=logging.INFO, handlers=[logging.StreamHandler()])
+    testExporter()
