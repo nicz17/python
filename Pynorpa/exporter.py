8d9b4220c720bd7d11ad51a744dd473c890fc59e
nicz17 <nicolas.zwahlen@gmx.net>
2024-11-21 18:41:07 +0100

    Add links and more bibliography

--- Pynorpa/exporter.py
+++ Pynorpa/exporter.py
@@ -35,8 +35,39 @@ class Exporter():
         """Build Links page"""
         page = pynorpaHtml.PynorpaHtmlPage('Liens')
         page.addHeading(1, 'Liens')
-        page.addHeading(2, 'Bibliographie')
+		
+        page.addHeading(2, "Insectes")
+        links = []
+        links.append(LinkHtmlTag("http://www.insecte.org/forum/", "Le monde des insectes - forum", True))
+        links.append(LinkHtmlTag("http://www.galerie-insecte.org/galerie/fichier.php", 
+                "Le monde des insectes - galerie", True))
+        links.append(LinkHtmlTag("http://www.galerie-insecte.org/galerie/auteur.php?aut=6169", 
+                "Le monde des insectes - mes photos", True))
+        links.append(LinkHtmlTag("http://www.quelestcetanimal.com/", "Quel est cet animal ?", True))
+        links.append(LinkHtmlTag("http://lepus.unine.ch/carto/", "InfoFauna UniNe", True))
+        links.append(LinkHtmlTag("http://www.lepido.ch/", "Papillons diurnes de Suisse", True))
+        links.append(LinkHtmlTag("http://cle.fourmis.free.fr/castes-fourmis.html", "Fourmis de France", True))
+        links.append(LinkHtmlTag("http://home.hccnet.nl/mp.van.veen/conopidae/ConGenera.html", "Clé des Conopidae", True))
+        page.addList(links)
 
+        page.addHeading(2, "Plantes")
+        links = []
+        links.append(LinkHtmlTag("http://www.visoflora.com/", "Visoflora", True))
+        links.append(LinkHtmlTag("https://www.infoflora.ch/fr/", "Infoflora - flore de Suisse", True))
+        links.append(LinkHtmlTag("http://www.tela-botanica.org/bdtfx-nn-60585-synthese", "Tela botanica", True))
+        page.addList(links)
+
+        page.addHeading(2, "Autres")
+        links = []
+        links.append(LinkHtmlTag("http://www.inaturalist.org/observations/nicz", "iNaturalist", True))
+        links.append(LinkHtmlTag("http://www.salamandre.net/", "La Salamandre - la revue des curieux de nature", True))
+        links.append(LinkHtmlTag("http://www.pronatura-vd.ch/nos_reserves", "Réserves Pro Natura Vaud", True))
+        links.append(LinkHtmlTag("http://www.arocha.ch/fr/projects/aide-entretien-pps/", "A Rocha - entretien de prairies sèches", True))
+        links.append(LinkHtmlTag("http://www.ornitho.ch/index.php?m_id=1", "Plate-forme ornithologique suisse", True))
+        links.append(LinkHtmlTag("https://www.thunderforest.com/", "Cartes par Thunderforest et OpenLayers", True))
+        page.addList(links)
+
+        page.addHeading(1, 'Bibliographie')
         biblio = []
         self.addBiblioRef(biblio, "P. Leraut, P. Blanchot", "Le guide entomologique", "Delachaux et Niestlé", "2012")
         self.addBiblioRef(biblio, "R. Dajoz", "Dictionnaire d'entomologie", "Lavoisier", "2010")
@@ -49,6 +80,8 @@ class Exporter():
         self.addBiblioRef(biblio, "E. Gerber, G. Kozlowski, A.-S. Mariéthoz", "La flore des Préalpes", "Rossolis", "2010")
         self.addBiblioRef(biblio, "F. Dupont, J.-L. Guignard", "Botanique, les familles de plantes", "15e édition, Elsevier Masson", "2012")
         self.addBiblioRef(biblio, "Collectif", "Les guides Salamandre", "Editions de la Salamandre, Neuchâtel", None)
+        self.addBiblioRef(biblio, "E. Sardet, C. Roesti, Y. Braud", "Orthoptères de France, Belgique, Luxembourg et Suisse", "Biotope", "2024")
+        self.addBiblioRef(biblio, "V. Hugonnot, F. Pépin, J. Celle", "Mousses et hépatiques de France", "Biotope", "2022")
         page.addList(biblio)
 
         page.save('export/links.html')
