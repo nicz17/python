7b2a5e005828ab7d3931bd7386ccd2f8b0339874
nicz17 <nicolas.zwahlen@gmx.net>
2025-02-11 22:03:23 +0100

    Work on picture insertion

--- Pynorpa/pynorpaManager.py
+++ Pynorpa/pynorpaManager.py
@@ -11,7 +11,7 @@ import os
 import DateTools
 from LocationCache import Location
 from PhotoInfo import PhotoInfo
-from picture import Picture
+from picture import Picture, PictureCache
 from taxon import TaxonCache
 
 class PynorpaManager():
@@ -34,17 +34,23 @@ class PynorpaManager():
     def init(self):
         """Initialize members."""
         self.taxonCache = TaxonCache()
+        self.pictureCache = PictureCache()
 
     def addPicture(self, filename: str, loc: Location) -> Picture:
         """Add a new Picture to gallery."""
         self.log.info('Adding picture %s at %s', filename, loc)
-        # TODO check picture is not added yet
+        # Check picture is not added yet
+        basename = os.path.basename(filename)
+        pic = self.pictureCache.findByName(basename)
+        if pic:
+            self.log.error('Picture is already in gallery: %s', pic)
+            return None
         info = PhotoInfo(filename)
         info.identify()
         tShotAt = DateTools.timestampToDatetimeUTC(info.getShotAt())
-        taxon = self.taxonCache.findByFilename(os.path.basename(filename))
+        taxon = self.taxonCache.findByFilename(basename)
         if taxon:
-            pic = Picture(-1, filename, tShotAt, None, taxon.getIdx(), DateTools.nowDatetime(), loc.getIdx(), 3)
+            pic = Picture(-1, basename, tShotAt, None, taxon.getIdx(), DateTools.nowDatetime(), loc.getIdx(), 3)
             pic.taxon = taxon
             pic.location = loc
             self.log.info('Will add %s', pic)
@@ -53,3 +59,14 @@ class PynorpaManager():
             self.log.error('Failed to find taxon for %s', filename)
             # TODO GUI error dialog
             return None
+        
+def testManager():
+    """Unit test for manager"""
+    mgr = PynorpaManager()
+    mgr.log.info('Testing PynorpaManager')
+    mgr.addPicture('vanessa-cardui004.jpg', None)
+
+if __name__ == '__main__':
+    logging.basicConfig(format="%(levelname)s %(name)s: %(message)s",
+        level=logging.INFO, handlers=[logging.StreamHandler()])
+    testManager()
