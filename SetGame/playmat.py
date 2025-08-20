7dbc28bc4feaf827e8bce1be30a2ef76ead54a00
nicz17 <nicolas.zwahlen@gmx.net>
2025-08-20 22:36:37 +0200

    fix: card highlight rect, fonts

--- SetGame/playmat.py
+++ SetGame/playmat.py
@@ -16,7 +16,7 @@ class Playmat():
     """A Set game playmat."""
     log = logging.getLogger('Playmat')
     colorBg = '#006400'
-    colorFg = '#107410'
+    colorFg = '#127812'
     colorBd = '#004000'
     cardw = 200
     cardh = 300
@@ -35,6 +35,7 @@ class Playmat():
 
     def addCards(self, cards: list[Card]):
         """Display cards on the playmat."""
+        # TODO clear card images first!
         self.log.info(f'Adding {len(cards)} cards to playmat')
         for iCard, card in enumerate(cards):
             filename = self.renderer.getImageFilename(card)
@@ -43,14 +44,14 @@ class Playmat():
             self.cardImages[card] = img
             x = self.margin + ((iCard % 4)+1)*(self.cardw + self.margin) + 100
             y = self.margin + int(iCard/4)*(self.cardh + self.margin)
-            id = self.canvas.create_image(x, y, anchor=tk.NW, image=img)
+            self.canvas.create_image(x, y, anchor=tk.NW, image=img)
             self.cardPositions[card] = (x, y)
 
     def render(self):
         """Render the initial game state."""
         self.canvas.create_text(110, 100, fill=self.colorFg, font=self.fontBg, text='SET')
-        ty = self.height/2 + self.cardh/2 + 16
-        self.txtDeck = self.canvas.create_text(110, ty, fill=self.colorBd, text='Pioche')
+        ty = self.height/2 + self.cardh/2 + 18
+        self.txtDeck = self.canvas.create_text(110, ty, fill=self.colorFg, font=self.fontFg, text='Pioche')
         #self.renderCardRect(110, self.height/2)
         imgCardBack = PhotoImage(file=f'images/cardback.png')
         self.cardImages['back'] = imgCardBack
@@ -64,17 +65,17 @@ class Playmat():
         """Render a card placement rectangle."""
         self.canvas.create_rectangle(cx-self.cardw/2, cy-self.cardh/2, cx+self.cardw/2, cy+self.cardh/2, outline=self.colorBd)
 
-    def drawHighlight(self, x: int, y: int, color: str):
-        """Draw a highlight square at the specified grid position."""
-        w = 4
-        x0 = x -2
-        x1 = x + self.cardw +2
-        y0 = y -2
-        y1 = y + self.cardh +2
-        self.aHighlightIds.append(self.canvas.create_line(x0-2, y0, x1+2, y0, fill=color, width=w))
-        self.aHighlightIds.append(self.canvas.create_line(x0, y0, x0, y1+2,   fill=color, width=w))
-        self.aHighlightIds.append(self.canvas.create_line(x0, y1, x1, y1,     fill=color, width=w))
-        self.aHighlightIds.append(self.canvas.create_line(x1, y0, x1, y1+2,   fill=color, width=w))
+    def drawHighlight(self, card: Card, color: str):
+        """Draw a highlight rectangle for the specified card."""
+
+        # Draw highlight rectangle
+        (x, y) = self.cardPositions[card]
+        id = self.canvas.create_rectangle(x-4, y-4, x+self.cardw+4, y+self.cardh+4, fill=color, outline=color)
+        self.aHighlightIds.append(id)
+        
+        # Redraw card image
+        img = self.cardImages[card]
+        self.canvas.create_image(x, y, anchor=tk.NW, image=img)
 
     def reset(self):
         """Reset the playmat."""
@@ -91,8 +92,7 @@ class Playmat():
         card = self.getCardAt(event.x, event.y)
         self.log.info(f'Click at {event.x}:{event.y} is {card}')
         if card:
-            pos = self.cardPositions[card]
-            self.drawHighlight(pos[0], pos[1], 'red')
+            self.drawHighlight(card, 'red')
             self.cbkCardSelection(card)
 
     def getCardAt(self, x: int, y: int) -> Card:
@@ -114,4 +114,5 @@ class Playmat():
 
         # Fonts
         self.fontBg = tkfont.Font(family="Helvetica", size=42)
+        self.fontFg = tkfont.Font(family="Helvetica", size=16, weight='bold')
         
