332f8494cf4ada4d195c2f5f0ea5048d31a399ab
nicz17 <nicolas.zwahlen@gmx.net>
2025-09-29 21:56:02 +0200

    feat: draft for Books module and manager

---
+++ Pynorpa/bookManager.py
@@ -0,0 +1,188 @@
+"""Module bookManager"""
+
+__author__ = "Nicolas Zwahlen"
+__copyright__ = "Copyright 2025 N. Zwahlen"
+__version__ = "1.0.0"
+
+import logging
+
+from taxon import Taxon
+from LocationCache import Location
+from picture import Picture
+
+
+class Book():
+    """A printed picture book."""
+    log = logging.getLogger("Book")
+
+    def __init__(self, name: str, desc: str):
+        """Constructor."""
+        self.name = name
+        self.desc = desc
+        self.status = 'Projet'
+        self.pictures = []
+
+    def getName(self) -> str:
+        """Getter for name"""
+        return self.name
+
+    def getDesc(self) -> str:
+        """Getter for desc"""
+        return self.desc
+
+    def getStatus(self) -> str:
+        """Getter for status"""
+        return self.status
+
+    def addPicture(self, pic: Picture):
+        """Add picture."""
+        self.pictures.append(pic)
+
+    def getPictures(self) -> list:
+        """Getter for pictures"""
+        return self.pictures
+
+    def save(self):
+        """Save."""
+        # TODO: implement save
+        pass
+
+    def toJson(self):
+        """Create a dict of this Book for json export."""
+        data = {
+            'name': self.name,
+            'desc': self.desc,
+            'status': self.status,
+            'pictures': self.pictures
+        }
+        return data
+
+    def __str__(self):
+        return f'Book {self.name} {self.status}'
+
+
+class BookManager():
+    """Class BookManager"""
+    log = logging.getLogger("BookManager")
+
+    def __init__(self):
+        self.books = []
+
+    def addBook(self):
+        """Add book."""
+        # TODO: implement addBook
+        pass
+
+    def getBooks(self) -> list[Book]:
+        return self.books
+
+    def loadBooks(self):
+        """Load books."""
+        # TODO: implement loadBooks
+        pass
+
+    def saveBooks(self):
+        """Save books."""
+        # TODO: implement saveBooks
+        pass
+
+    def toHtml(self, book: Book):
+        """Export a book as html preview."""
+        # TODO: implement toHtml
+        pass
+
+    def findOriginal(self, pic: Picture):
+        """Find original picture."""
+        # TODO: implement findOriginal
+        pass
+
+    def toJson(self):
+        """Create a dict of this BookManager for json export."""
+        data = {
+            'books': self.books,
+        }
+        return data
+
+    def __str__(self):
+        str = "BookManager"
+        str += f' books: {self.books}'
+        return str
+
+
+class BookPicFilter():
+    """Class BookPicFilter"""
+    log = logging.getLogger("BookPicFilter")
+
+    def __init__(self):
+        """Constructor."""
+        self.taxon = None
+        self.location = None
+        self.quality = 4
+
+    def getTaxon(self) -> Taxon:
+        """Getter for taxon"""
+        return self.taxon
+
+    def setTaxon(self, taxon: Taxon):
+        """Setter for taxon"""
+        self.taxon = taxon
+
+    def getLocation(self) -> Location:
+        """Getter for location"""
+        return self.location
+
+    def setLocation(self, location: Location):
+        """Setter for location"""
+        self.location = location
+
+    def getQuality(self) -> int:
+        """Getter for quality"""
+        return self.quality
+
+    def setQuality(self, quality: int):
+        """Setter for quality"""
+        self.quality = quality
+
+    def __str__(self):
+        str = "BookPicFilter"
+        str += f' taxon: {self.taxon}'
+        str += f' location: {self.location}'
+        str += f' quality: {self.quality}'
+        return str
+
+
+def testBook():
+    """Unit test for Book"""
+    Book.log.info("Testing Book")
+    book = Book("nameExample", "descExample")
+    book.log.info(book)
+    book.getName()
+    book.getDesc()
+    book.getStatus()
+    book.getPictures()
+    book.save()
+    book.log.info(book.toJson())
+
+def testBookManager():
+    """Unit test for BookManager"""
+    BookManager.log.info("Testing BookManager")
+    mgr = BookManager()
+    mgr.loadBooks()
+
+def testBookPicFilter():
+    """Unit test for BookPicFilter"""
+    BookPicFilter.log.info("Testing BookPicFilter")
+    book = BookPicFilter()
+    book.log.info(book)
+    book.BookPicFilter()
+    book.getTaxon()
+    book.getLocation()
+    book.getQuality()
+    book.log.info(book.toJson())
+
+if __name__ == '__main__':
+    logging.basicConfig(format="%(levelname)s %(name)s: %(message)s",
+        level=logging.INFO, handlers=[logging.StreamHandler()])
+    testBook()
+    testBookManager()
+    testBookPicFilter()
--- Pynorpa/moduleBooks.py
+++ Pynorpa/moduleBooks.py
@@ -15,7 +15,11 @@ with iFolor or smartphoto.ch
 import config
 import logging
 
+import BaseWidgets
+import tkinter as tk
+from tkinter import ttk
 from TabsApp import TabModule, TabsApp
+from bookManager import Book, BookPicFilter
 
 
 class ModuleBooks(TabModule):
@@ -27,3 +31,78 @@ class ModuleBooks(TabModule):
         self.window = parent.window
         super().__init__(parent, 'Livres')
         self.book = None
+
+    def loadData(self):
+        self.book = Book('Test01', 'Livre test 1')
+        self.onSelectBook(self.book)
+
+    def onSelectBook(self, book: Book):
+        """Display selected book in editor."""
+        self.log.info(f'Selected {book}')
+        self.book = book
+        self.bookEditor.loadData(book)
+
+    def onSaveBook(book: Book):
+        pass
+
+    def createWidgets(self):
+        """Create user widgets."""
+        self.createLeftRightFrames()
+
+        # Filter on location, taxon, quality
+        frmFilter = ttk.LabelFrame(self.frmLeft, text='Filtres')
+        frmFilter.pack(fill=tk.X)
+        self.lblFilter = ttk.Label(frmFilter, text='Filtrer par lieu, taxon et qualité')
+        self.lblFilter.pack(fill=tk.X)
+
+        # Table of filtered photos
+
+
+        # Book selector
+
+
+        # Book editor
+        self.bookEditor = BookEditor(self.onSaveBook)
+        self.bookEditor.createWidgets(self.frmRight)
+
+        # Table of pics in selected Book
+
+
+        # Buttons
+
+
+class BookEditor(BaseWidgets.BaseEditor):
+    """Class BookEditor"""
+    log = logging.getLogger("BookEditor")
+
+    def __init__(self, cbkSave):
+        """Constructor."""
+        super().__init__(cbkSave, '#62564f')
+        self.book = None
+
+    def loadData(self, book: Book):
+        """Display the specified book in this editor."""
+        self.book = book
+        self.setValue(book)
+
+    def createWidgets(self, parent: tk.Frame):
+        """Add the editor widgets to the parent widget."""
+        super().createWidgets(parent, 'Propriétés du livre')
+        
+        self.widName   = self.addText('Nom', Book.getName)
+        self.widDesc   = self.addText('Description', Book.getDesc)
+        self.widStatus = self.addText('Status', Book.getStatus)
+        
+        self.createButtons(True, True, False)
+        #self.btnUpload = self.addButton('Publier', self.onUpload, 'go-up')
+        self.enableWidgets()
+
+    def enableWidgets(self, evt=None):
+        """Enable our internal widgets."""
+        editing  = self.book is not None
+        modified = self.hasChanges(self.book)
+        super().enableWidgets(editing)
+        self.enableButtons(modified, modified, False)
+
+    def __str__(self):
+        return 'BookEditor'
