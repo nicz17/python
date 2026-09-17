f0c6794bed5512b4d6eaca0a1f610038513b0c23
nicz17 <nicolas.zwahlen@gmx.net>
2026-09-17 20:35:23 +0200

    feat: more parameters in NameGen constructor

--- Common/NameGen.py
+++ Common/NameGen.py
@@ -1,5 +1,5 @@
 """
- A name generation tool
+ A name generation tool.
 """
 
 __author__ = "Nicolas Zwahlen"
@@ -11,16 +11,19 @@ import random
 class NameGen:
     """A simple random name generator."""
 
-    def __init__(self, seed=42):
-        """Constructor with optional seed."""
+    def __init__(self, seed=42, minSyl=2, maxSyl=4, flipProb=0.06):
+        """Constructor with optional seed, min-amx syllables and syllable flip probability."""
         self.seed = seed
+        self.minSyl = minSyl
+        self.maxSyl = maxSyl
+        self.flipProb = flipProb
         random.seed(seed)
         self.vowels = ['a', 'e', 'i', 'o', 'u', 'y']
         self.consos = ['b', 'c', 'd', 'f', 'g', 'h', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
 
     def generate(self):
         """Generates and returns a random name."""
-        nSyllables = random.randint(2, 4)
+        nSyllables = random.randint(self.minSyl, self.maxSyl)
         sName = ''
         for s in range(nSyllables):
             sName += self.getSyllable()
@@ -28,11 +31,9 @@ class NameGen:
 
     def getSyllable(self):
         """Returns a random syllable."""
-        rPermutation = random.random()
-        if (rPermutation < 0.06):
+        rflip = random.random()
+        if rflip < self.flipProb:
             return self.getVowel() + self.getConsonant()
-        elif (rPermutation < 0.12):
-            self.getConsonant() + self.getConsonant() + self.getVowel()
         return self.getConsonant() + self.getVowel()
 
     def getVowel(self):
@@ -43,17 +44,27 @@ class NameGen:
         """Returns a random consonant."""
         return random.choice(self.consos)
 
-def testNameGen():
+    def __str__(self):
+        return f'NameGen seed {self.seed}, {self.minSyl}-{self.maxSyl} syllables, {self.flipProb} fliprate'
+
+
+def testGrid(ng: NameGen):
     """Tests the name generator by printing several names to console."""
     nCols = 8
     nRows = 6
-    ng = NameGen(random.randint(0, 42))
+    print(ng)
     for iCol in range(nCols):
         sRow = ''
         for iRow in range(nRows):
             sName = ng.generate()
             sRow += sName.ljust(10)
         print(sRow)
+    print()
+
+def testNameGen():
+    """Unit test for NameGen."""
+    testGrid(NameGen(random.randint(0, 42)))
+    testGrid(NameGen(random.randint(0, 42), 3, 3, 0.01))
 
 if __name__ == '__main__':
     testNameGen()
